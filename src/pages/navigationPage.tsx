a741ae0986d80c5c5b1fd9db129bafd32e12d18c
skyhanull <anky0425@naver.com>
2023-04-13 01:20:16 +0900

    refacor: 메인으로 돌아가는 로고 생성

--- src/pages/navigationPage.tsx
+++ src/pages/navigationPage.tsx
@@ -1,4 +1,4 @@
-import { Badge, Box, Button, Stack, Flex, Spacer, Text } from '@chakra-ui/react'
+import { Box, Flex, Text } from '@chakra-ui/react'
 import { useSelector } from 'react-redux'
 import { Link, Outlet } from 'react-router-dom'
 import { RootState } from '../store/store'
@@ -6,13 +6,27 @@ import { RootState } from '../store/store'
 function NavigationPage() {
   const ListAllData = useSelector((state: RootState) => state.musicSlice)
   return (
-    <Flex display='flex' flexDirection='row'>
-      <Box height='100vh' width='250px' display='flex' flexDirection='row'>
-        <Link to='/main' style={{ height: '100%', padding: '25px' }}>
-          <img src={`${ListAllData.result.icon.label}`}></img>
+    <Flex>
+      <Box height='100vh' width='20%' backgroundColor='gray.300'>
+        <Link to='/'>
+          <Box
+            width='100%'
+            display='flex'
+            justifyContent='center'
+            flexDirection='row'
+          >
+            <img
+              src={`${ListAllData.result.icon.label}`}
+              height='40px'
+              width='40px'
+            ></img>
+            <Text as='b' fontSize='28px'>
+              ELMusic
+            </Text>
+          </Box>
         </Link>
-        <Text>Music</Text>
       </Box>
+
       <Outlet />
     </Flex>
   )
