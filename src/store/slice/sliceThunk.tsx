028f853d7bbe20675af25722a9f015fca0b976a0
skyhanull <anky0425@naver.com>
2023-04-12 20:01:19 +0900

    feat: redux-thunk생성

---
+++ src/store/slice/sliceThunk.tsx
@@ -0,0 +1,13 @@
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import getInfo from '../../apis/Api'
+
+const thunkApi = createAsyncThunk('api', async () => {
+  try {
+    const response = await getInfo()
+    return response.entry
+  } catch {
+    throw new Error()
+  }
+})
+
+export default thunkApi
