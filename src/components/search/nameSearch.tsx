abaec4673857ebb27d0b0e06f3ebe7ef6ad510c8
skyhanull <anky0425@naver.com>
2023-04-12 22:55:00 +0900

    add: reset버튼 생성

---
+++ src/components/search/nameSearch.tsx
@@ -0,0 +1,46 @@
+import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
+import { useState } from 'react'
+import { KeyboardEvent } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { CloseIcon } from '@chakra-ui/icons'
+
+function NameSearch() {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const [name, setName] = useState('')
+
+  const keypressHandler = () => {
+    searchParams.set('name', name)
+    setSearchParams(searchParams)
+  }
+
+  const submitHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      keypressHandler()
+    }
+  }
+
+  const resetHandler = () => {
+    setName('')
+    searchParams.set('name', name)
+    setSearchParams(searchParams)
+  }
+  return (
+    <div>
+      <InputGroup>
+        <Input
+          w='300px'
+          type='text'
+          placeholder='Search'
+          onChange={e => setName(e.target.value)}
+          defaultValue={name}
+          onKeyPress={submitHandler}
+        />
+        <InputLeftElement>
+          <CloseIcon onClick={resetHandler} />
+        </InputLeftElement>
+      </InputGroup>
+    </div>
+  )
+}
+
+export default NameSearch
