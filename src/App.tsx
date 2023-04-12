aa80391b5d77a611b8f8489f60612a16f30bb85c
skyhanull <anky0425@naver.com>
2023-04-12 22:54:17 +0900

    add: 페이지들 router에 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import MainPage from './pages/mainPage'
 import tripApi from './store/slice/sliceThunk'
 import { useDispatch } from 'react-redux'
 import { AppDispatch } from './store/store'
+import NotFound from './pages/notFoundPage'
+import NavigationPage from './pages/navigationPage'
+import SubInfoPage from './pages/subInfoPage'
 
 function App() {
   const dispatch = useDispatch<AppDispatch>()
@@ -16,10 +19,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<MainPage />}>
-          <Route path='/:id' element={<MainPage />} />
+        <Route path='/' element={<NavigationPage />}>
+          <Route path='/' element={<MainPage />} />
+          <Route path='/:id' element={<SubInfoPage />} />
         </Route>
-        {/* <Route path="*" element={<NotFound />} /> */}
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
