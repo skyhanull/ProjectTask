57ec8bd426a081046cca230f176f11c105dae316
skyhanull <anky0425@naver.com>
2023-04-13 04:15:32 +0900

    design: px 를 rem으로 변경

--- src/components/list/topChart.tsx
+++ src/components/list/topChart.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Ientryinfo from '../../types'
-import { Table, Thead, Tbody, Tr, Th, Td, Image } from '@chakra-ui/react'
+import { Table, Thead, Tbody, Tr, Th, Td, Image, Flex } from '@chakra-ui/react'
 import SortButton from '../sort/sortButton'
 import { useNavigate } from 'react-router-dom'
 
@@ -12,32 +12,28 @@ function TopChart({ data }: { data: Ientryinfo[] }) {
   }
 
   return (
-    <Table
-      variant='striped'
-      colorScheme='gray'
-      size='sm'
-      flexDirection='column'
-    >
+    <Table variant='striped' colorScheme='gray' size='sm'>
       <Thead>
         <Tr display='flex'>
-          <Th width='230px' display='flex' alignItems='center'>
+          <Th width='17rem' display='flex' alignItems='center'>
             Song <SortButton />
           </Th>
           <Th></Th>
-          <Th width='200px' display='flex' alignItems='center'>
+          <Th width='15rem' display='flex' alignItems='center'>
             Artist
           </Th>
-          <Th width='200px' display='flex' alignItems='center'>
+          <Th width='10rem' display='flex' alignItems='center'>
             itemCount
           </Th>
-          <Th width='150px' display='flex' alignItems='center'>
+          <Th width='10rem' display='flex' alignItems='center'>
             price
           </Th>
-          <Th width='200px' display='flex' alignItems='center'>
+          <Th width='10rem' display='flex' alignItems='center'>
             releaseDate
           </Th>
         </Tr>
       </Thead>
+
       <Tbody>
         {data.map((item, idx) => (
           <Tr
@@ -49,15 +45,15 @@ function TopChart({ data }: { data: Ientryinfo[] }) {
             <Td>
               <Image src={`${item['im:image'][0].label}`} height='35px'></Image>
             </Td>
-            <Td width='200px' overflow='hidden'>
+            <Td width='15rem' overflow='hidden'>
               {item['im:name'].label}
             </Td>
-            <Td width='200px' overflow='hidden'>
+            <Td width='15rem' overflow='hidden'>
               {item['im:artist'].label}
             </Td>
-            <Td width='200px'>{item['im:itemCount'].label}</Td>
-            <Td width='150px'>{item['im:price'].label}</Td>
-            <Td width='180px'>{item['im:releaseDate'].attributes.label}</Td>
+            <Td width='10rem'>{item['im:itemCount'].label}</Td>
+            <Td width='10rem'>{item['im:price'].label}</Td>
+            <Td width='13rem'>{item['im:releaseDate'].attributes.label}</Td>
           </Tr>
         ))}
       </Tbody>
